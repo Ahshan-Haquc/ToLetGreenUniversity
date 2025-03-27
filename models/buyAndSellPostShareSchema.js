d95633a3551bedac99aea352592721e6e351ef4f
Ahshan-Haquc <ahshanulhaqucgub@gmail.com>
2025-03-27 00:07:37 -0700

    all frontend work done of buyAndSell and do post router work all done except image upload

---
+++ models/buyAndSellPostShareSchema.js
@@ -0,0 +1,89 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+      },
+      category: {
+        type: String,
+        required: true,
+        // enum: ['Electronics', 'Furniture', 'Computer', 'Phone', 'Other'] // Modify categories as needed
+      },
+      price: {
+        type: Number,
+        required: true,
+        min: 0
+      },
+      condition: {
+        type: String,
+        required: true,
+        // enum: ['New', 'Used', 'Like New']
+      },
+      description: {
+        type: String,
+        trim: true
+      },
+      contact: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String,
+        required: true
+      },
+      images: {
+        type: [String], // Array of image URLs
+        validate: [arrayLimit, 'You can upload up to 3 images']
+      },
+      postDate: {
+        type: Date,
+        default: Date.now
+      },
+    available: {
+        type: String,
+        enum: ["yes", "no"],
+        default: "yes",
+    },
+    likeCount: {
+        type: Number,
+        default: 0,
+    },
+    dislikeCount: {
+        type: Number,
+        default: 0,
+    },
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'StudentInformation'
+        }
+    ],
+    dislikes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'StudentInformation'
+        }
+    ],
+    // Virtual field to calculate review score based on like and dislike ratio
+    reviewScore: {
+        type: Number,
+        default: 3,  // Default to neutral rating
+        min: 1,
+        max: 5
+    },
+    studentPostedId: {
+        type: mongoose.Types.ObjectId,
+        ref: "StudentInformation",
+    }
+});
+
+// function for check if user uploaded more then 3 pictures or not 
+function arrayLimit(val) {
+    return val.length <= 5;
+  }
+
+const postModel = mongoose.model("BuyAndSellPostShare", postSchema);
+
+module.exports = postModel;
--- routes/buyAndSellRouter.js
+++ routes/buyAndSellRouter.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const buySellRouter = express.Router();
 const accessPermission = require("../middlewares/accessPermission");
+const upload = require("../middlewares/uploadImages");
+const ratingCalculate = require("../controller/ratingCalculate");
+const BuySellModel = require("../models/buyAndSellPostShareSchema")
 
 buySellRouter.get('/buyAndSellHomePage',accessPermission,(req,res)=>{
     try {
         res.status(200).render("buyAndSell/buyAndSellHomePage",{
-            student: req.studentInfo
+            student: req.studentInfo,
+            comeFromFilterRouter: false,
         });
     } catch (error) {
         console.log("Error in lost and found home page get router.");
@@ -13,13 +17,55 @@ buySellRouter.get('/buyAndSellHomePage',accessPermission,(req,res)=>{
     }
 })
 
-buySellRouter.get('/buyAndSellSeePost',accessPermission,(req,res)=>{
+buySellRouter.get('/buyAndSellSeePost',accessPermission,async(req,res)=>{
     try {
+        const availablePosts = await BuySellModel.find({available:"yes"});
+
         res.status(200).render("buyAndSell/buyAndSellSeePost",{
-            student: req.studentInfo
+            student: req.studentInfo,
+            totalSeatAvailable: availablePosts
         });
     } catch (error) {
         console.log("Error in lost and found home page get router.");
+        console.log(error);
+    }
+})
+
+// router for doing post 
+buySellRouter.get('/buyAndSellDoPost',accessPermission,(req,res)=>{
+    try {
+        res.status(200).render("buyAndSell/buyAndSellDoPost",{
+            student: req.studentInfo
+        });
+    } catch (error) {
+        console.log("Error in lost and found do post page get router.");
+        console.log(error);
+    }
+})
+buySellRouter.post('/buyAndSellDoPost',accessPermission,  upload.array('images', 5),async(req,res)=>{
+    try {
+        // const values = req.body;
+        // Map uploaded files to get paths
+      const imagePaths = req.files.map(file => file.path);
+
+      const PostInfo = new BuySellModel({
+        title: req.body.title,
+        category: req.body.category,
+        price:req.body.price,
+        condition: req.body.condition,
+        description: req.body.description,
+        contact: req.body.contact,
+        location: req.body.location,
+        images: imagePaths,
+        studentPostedId: req.studentInfo._id
+      })
+
+      await PostInfo.save();
+
+        console.log("Sell post form submitted successfully");
+        res.status(200).redirect('/buyAndSellHomePage');
+    } catch (error) {
+        console.log("Error in lost and found do post page POST router.");
         console.log(error);
     }
 })
